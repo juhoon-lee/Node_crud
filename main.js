e3452c1026a0b3dda7159d81b5be467da0a0fcca
juhoon <wngns8334@gmail.com>
2021-11-07 02:41:24 +0900

    :wq

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var db = mysql.createConnection({
 db.connect();
 
 let loginCheck = false;
-let id = '';
+let author = '';
 let name = '';
 
 var app = http.createServer(function (request, response) {
@@ -20,19 +20,56 @@ var app = http.createServer(function (request, response) {
 	var queryData = url.parse(_url, true).query;
 	var pathname = url.parse(_url, true).pathname;
 	if (pathname === '/') {
-		db.query(`SELECT * FROM board`, function (error, boards) {
+		if (queryData.id === undefined) {
+			db.query(`SELECT * FROM board`, function (error, boards) {
+				if (loginCheck === true) {
+					var list = template.list(boards);
+					var html = template.loginHTML(name, list);
+
+					response.writeHead(200);
+					response.end(html);
+				} else {
+					var list = template.list(boards);
+					var html = template.HTML(list);
+					response.writeHead(200);
+					response.end(html);
+				}
+			});
+		} else {
 			if (loginCheck === true) {
-				var list = template.list(boards);
-				var html = template.loginHTML(name, list);
-				response.writeHead(200);
-				response.end(html);
+				db.query(`SELECT * FROM board WHERE id=?`, [queryData.id], function (error, boards) {
+					db.query(
+						`SELECT * FROM comment WHERE board_id=?`,
+						[queryData.id],
+						function (error, comments) {
+							if (error) {
+								throw error;
+							}
+							const comentList = template.Cmlist(comments);
+							const html = template.LoginShowBoard(boards, comentList);
+							response.writeHead(200);
+							response.end(html);
+						}
+					);
+				});
 			} else {
-				var list = template.list(boards);
-				var html = template.HTML(list);
-				response.writeHead(200);
-				response.end(html);
+				db.query(`SELECT * FROM board WHERE id=?`, [queryData.id], function (error, boards) {
+					db.query(
+						`SELECT * FROM comment WHERE board_id=?`,
+						[queryData.id],
+						function (error, comments) {
+							if (error) {
+								throw error;
+							}
+							const comentList = template.Cmlist(comments);
+							const html = template.ShowBoard(boards, comentList);
+							response.writeHead(200);
+							response.end(html);
+						}
+					);
+				});
 			}
-		});
+		}
 	} else if (pathname === '/login') {
 		const html = template.Login();
 		response.writeHead(200);
@@ -48,14 +85,14 @@ var app = http.createServer(function (request, response) {
 				if (error) {
 					throw error;
 				}
-				console.log(result);
 				for (let i = 0; i < result.length; i++) {
 					if (result[i].id === post.id && result[i].pw === post.password) {
 						loginCheck = true;
+						author = result[i].id;
 					}
 				}
 				if (loginCheck === true) {
-					id = post.name;
+					name = post.id;
 					response.writeHead(302, { Location: `/` });
 					response.end();
 				} else {
@@ -66,7 +103,7 @@ var app = http.createServer(function (request, response) {
 		});
 	} else if (pathname === '/logout') {
 		loginCheck = false;
-		id = '';
+		author = '';
 		name = '';
 		response.writeHead(302, { Location: `/` });
 		response.end();
@@ -106,47 +143,35 @@ var app = http.createServer(function (request, response) {
 			const post = qs.parse(body);
 			db.query(
 				`
-          INSERT INTO topic (title, description, created, author_id)
-          VALUES(?, ?, NOW(), ?)`,
-				[post.title, post.description, 1],
+          INSERT INTO board (title, text, author)
+          VALUES(?, ?, ?)`,
+				[post.title, post.text, author],
 				function (error, result) {
 					if (error) {
 						throw error;
 					}
+					console.log(author);
 					response.writeHead(302, { Location: `/?id=${result.insertId}` });
 					response.end();
 				}
 			);
 		});
 	} else if (pathname === '/update') {
-		db.query('SELECT * FROM topic', function (error, topics) {
-			if (error) {
-				throw error;
-			}
-			db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function (error2, topic) {
-				if (error2) {
-					throw error2;
+		var body = '';
+		request.on('data', function (data) {
+			body = body + data;
+		});
+		request.on('end', function () {
+			var post = qs.parse(body);
+			db.query(`SELECT * FROM board WHERE id=?`, [post.id], function (error, boards) {
+				if (boards[0].author === author) {
+					const html = template.Update(boards);
+					response.writeHead(200);
+					response.end(html);
+				} else {
+					response.writeHead(302, { Location: `/?id=${post.id}` });
+					response.end();
 				}
-				var list = template.list(topics);
-				var html = template.HTML(
-					topic[0].title,
-					list,
-					`
-          <form action="/update_process" method="post">
-            <input type="hidden" name="id" value="${topic[0].id}">
-            <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-            <p>
-            <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-            </p>
-            <p>
-            <input type="submit">
-            </p>
-          </form>
-          `,
-					`<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-				);
-				response.writeHead(200);
-				response.end(html);
 			});
 		});
 	} else if (pathname === '/update_process') {
@@ -157,9 +182,12 @@ var app = http.createServer(function (request, response) {
 		request.on('end', function () {
 			var post = qs.parse(body);
 			db.query(
-				'UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?',
-				[post.title, post.description, post.id],
+				'UPDATE board SET title=?, text=? WHERE id=?',
+				[post.title, post.text, post.id],
 				function (error, result) {
+					if (error) {
+						throw error;
+					}
 					response.writeHead(302, { Location: `/?id=${post.id}` });
 					response.end();
 				}
@@ -172,13 +200,37 @@ var app = http.createServer(function (request, response) {
 		});
 		request.on('end', function () {
 			var post = qs.parse(body);
-			db.query('DELETE FROM topic WHERE id = ?', [post.id], function (error, result) {
-				if (error) {
-					throw error;
-				}
-				response.writeHead(302, { Location: `/` });
+			if (post.author === author) {
+				db.query('DELETE FROM board WHERE id=?', [post.id], function (error, result) {
+					if (error) {
+						throw error;
+					}
+					response.writeHead(302, { Location: `/` });
+					response.end();
+				});
+			} else {
+				response.writeHead(302, { Location: `/?id=${post.id}` });
 				response.end();
-			});
+			}
+		});
+	} else if (pathname === '/comment_process') {
+		var body = '';
+		request.on('data', function (data) {
+			body = body + data;
+		});
+		request.on('end', function () {
+			var post = qs.parse(body);
+			db.query(
+				'INSERT INTO comment(comment,author,board_id) VALUES(?, ?, ?)',
+				[post.comment, author, post.id],
+				function (error, result) {
+					if (error) {
+						throw error;
+					}
+					response.writeHead(302, { Location: `/?id=${post.id}` });
+					response.end();
+				}
+			);
 		});
 	} else {
 		response.writeHead(404);
